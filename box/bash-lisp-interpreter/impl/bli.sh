33a2a60bf35dfd97059b2790ff73087717345147
Reuben Garrett <ruby@rubydono.org>
2015-04-17 04:49:33 +0000

    Improved Reader
    Worked on the reader a bit. Considered iterating through the input string
    character-by-character under an elaborate case switch — however Bash
    features builtin support for Regular Expressions, which is likely
    to provide superior performance by using primitive operations where possible.

--- box/bash-lisp-interpreter/impl/bli.sh
+++ box/bash-lisp-interpreter/impl/bli.sh
@@ -7,15 +7,68 @@ bli-debug(){
 }
 
 bli-read(){
-    local -a input
-    if read -a input
+    local input
+    if read input
     then
-	echo "${input[@]}"
-	return 0
+	bli-debug "read: $input"
 
+	local -a output
+	local inQueue="$input"
+	local -i len="${#input}"
+	local -i idx=0
+	local -i inString=0 # are we currently evaluating a string literal?
+	local -i inEscape=0 # are we currently evaluating an escape sequence?
+	local currString=''
+	local currChar=''
+	local reStringChunk='^("[^"]*")(.*)$'
+	local reTokenChunk='^[[:space:]]*([^[:space:]"]+)(.*$)'
+	local reUntermString='^"[^"]*$'
+
+	# ( some tokens "and bits" 1 2 345 )
+
+	while (( ${#inQueue} > 0 ))
+	do
+
+	    if [[ "$inQueue" =~ $reUntermString ]]
+	    then
+		bli-debug "read error: unterminated string: $inQueue"
+		return 1
+	    else
+		bli-debug "unterminated string check returns: $?"
+	    fi
+
+	    if [[ "$inQueue" =~ $reStringChunk ]]
+	    then
+		output += "${BASH_REMATCH[1]}"
+		inQueue="${BASH_REMATCH[2]}"
+	    else
+		bli-debug "string check returns: $?"
+	    fi
+
+	    if [[ "$inQueue" =~ $reTokenChunk ]]
+	    then
+		bli-debug "reTokenChunk: ${BASH_REMATCH[1]}"
+		inQueue="${BASH_REMATCH[2]}"
+		bli-debug "inQueue: $inQueue"
+	    else
+		bli-debug "token check returns: $?"
+	    fi
+
+	done
+	
+	return
+	while (( idx < len ))
+	do
+	    currChar=${input:$idx:1}
+	    echo read: $currChar
+	    let idx++
+	done
+	return 0
+#	echo "${input[@]}"
+#	return 0
 	# TODO: not sure we really need to scope input ... 
-	re='^(declare -a )(.*)$'
-	if [[ $(declare -p BLI_INPUT) =~ $re ]]
+	re="^(declare -a input='\\()(.*)\\)'\$"
+	if [[ $(declare -p input) =~ $re ]]
 	then
 	    echo "${BASH_REMATCH[2]}"
 	fi
@@ -36,7 +89,8 @@ bli-eval(){
 	else
 	    if bli-symbol-p "${element}"
 	    then
-		bli-debug symbol: "${element}" $( bli-lookup-symbol "${element}" ${environment_name} )
+		bli-debug symbol: "${element}"
+		bli-lookup-symbol "${element}" ${environment_name}
 	    fi
 	fi
     done
@@ -108,8 +162,30 @@ bli-lookup-symbol(){
     fi
 }
 
+bli-test(){
+    echo bli-test'!'
+}
+
+bli-read-test(){
+    bli-read <<EOF
+( some tokens "and bits" 1 2 345 )
+some more tokens
+    leading whitespace
+"unterminated string
+EOF
+}
+
+while read -a command
+do
+      eval ${command[@]}
+done
+
+exit
+
+
 for i in {1..1}
 do
     echo "> "
     bli-print $(bli-eval 'BLI_GLOBAL_ENVIRONMENT' $(bli-read)) 
 done
+
